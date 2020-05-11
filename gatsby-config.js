cfe60b30d6d74882e4614f50fba6d7a6a15c8b19
Pawan Arora <pawan.arora@myoperator.co>
2020-05-12 01:37:40 +0530

    tutorial complete

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,25 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: `Pandas Eating Lots`,
+    description: `A simple description about pandas eating lots...`,
+    author: `Pawan Arora`
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      }
+    },
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-emotion`,
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
 }
---
+++ src/utils/typography.js
@@ -0,0 +1,7 @@
+import Typography from 'typography';
+import kirkham from 'typography-theme-kirkham';
+
+const typography = new Typography(kirkham);
+
+export default typography;
+export const rhythm = typography.rhythm;
